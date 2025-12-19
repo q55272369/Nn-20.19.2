46779fcc56720c8c99087ec8d451de57f51b76a7
q55272369 <q55272369@gmail.com>
2025-12-19 10:40:11 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,9 +9,9 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href=''
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        91狼友之家 {siteConfig('VERSION')}
       </a>
       .
     </div>
