46b690b92df0e2abec186b4dd636e3e260617a3f
q55272369 <q55272369@gmail.com>
2025-12-19 10:56:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '91狼友之家', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '你想看的这里都有', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '91狼友之家, 91论坛, 探花, 91porn, 狼友资源, 幼幼资源, 暗网资源, 迷奸资源, 狼友之家, 成人视频', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
